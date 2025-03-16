251c65ae3b7c25a5a83968fae40616f0e818dd3b
powell-newin <52061817+powell-newin@users.noreply.github.com>
2025-03-16 12:33:54 +0900

    Add endpoints option

--- plugins/updater/guest-js/index.ts
+++ plugins/updater/guest-js/index.ts
@@ -2,30 +2,32 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-License-Identifier: MIT
 
-import { invoke, Channel, Resource } from '@tauri-apps/api/core'
+import { Channel, invoke, Resource } from '@tauri-apps/api/core';
 
 /** Options used when checking for updates */
 interface CheckOptions {
   /**
    * Request headers
    */
-  headers?: HeadersInit
+  headers?: HeadersInit;
   /**
    * Timeout in milliseconds
    */
-  timeout?: number
+  timeout?: number;
   /**
    * A proxy url to be used when checking and downloading updates.
    */
-  proxy?: string
+  proxy?: string;
   /**
    * Target identifier for the running application. This is sent to the backend.
    */
-  target?: string
+  target?: string;
   /**
    * Allow downgrades to previous versions by not checking if the current version is greater than the available version.
    */
-  allowDowngrades?: boolean
+  allowDowngrades?: boolean;
+
+  endpoints?: string[];
 }
 
 /** Options used when downloading an update */
@@ -33,47 +35,47 @@ interface DownloadOptions {
   /**
    * Request headers
    */
-  headers?: HeadersInit
+  headers?: HeadersInit;
   /**
    * Timeout in milliseconds
    */
-  timeout?: number
+  timeout?: number;
 }
 
 interface UpdateMetadata {
-  rid: number
-  currentVersion: string
-  version: string
-  date?: string
-  body?: string
-  rawJson: Record<string, unknown>
+  rid: number;
+  currentVersion: string;
+  version: string;
+  date?: string;
+  body?: string;
+  rawJson: Record<string, unknown>;
 }
 
 /** Updater download event */
 type DownloadEvent =
-  | { event: 'Started'; data: { contentLength?: number } }
-  | { event: 'Progress'; data: { chunkLength: number } }
-  | { event: 'Finished' }
+  | { event: 'Started'; data: { contentLength?: number; }; }
+  | { event: 'Progress'; data: { chunkLength: number; }; }
+  | { event: 'Finished'; };
 
 class Update extends Resource {
   // TODO: remove this field in v3
   /** @deprecated This is always true, check if the return value is `null` instead when using {@linkcode check} */
-  available: boolean
-  currentVersion: string
-  version: string
-  date?: string
-  body?: string
-  rawJson: Record<string, unknown>
-  private downloadedBytes?: Resource
+  available: boolean;
+  currentVersion: string;
+  version: string;
+  date?: string;
+  body?: string;
+  rawJson: Record<string, unknown>;
+  private downloadedBytes?: Resource;
 
   constructor(metadata: UpdateMetadata) {
-    super(metadata.rid)
-    this.available = true
-    this.currentVersion = metadata.currentVersion
-    this.version = metadata.version
-    this.date = metadata.date
-    this.body = metadata.body
-    this.rawJson = metadata.rawJson
+    super(metadata.rid);
+    this.available = true;
+    this.currentVersion = metadata.currentVersion;
+    this.version = metadata.version;
+    this.date = metadata.date;
+    this.body = metadata.body;
+    this.rawJson = metadata.rawJson;
   }
 
   /** Download the updater package */
@@ -84,29 +86,29 @@ class Update extends Resource {
     convertToRustHeaders(options)
     const channel = new Channel<DownloadEvent>()
     if (onEvent) {
-      channel.onmessage = onEvent
+      channel.onmessage = onEvent;
     }
     const downloadedBytesRid = await invoke<number>('plugin:updater|download', {
       onEvent: channel,
       rid: this.rid,
       ...options
-    })
-    this.downloadedBytes = new Resource(downloadedBytesRid)
+    });
+    this.downloadedBytes = new Resource(downloadedBytesRid);
   }
 
   /** Install downloaded updater package */
   async install(): Promise<void> {
     if (!this.downloadedBytes) {
-      throw new Error('Update.install called before Update.download')
+      throw new Error('Update.install called before Update.download');
     }
 
     await invoke('plugin:updater|install', {
       updateRid: this.rid,
       bytesRid: this.downloadedBytes.rid
-    })
+    });
 
     // Don't need to call close, we did it in rust side already
-    this.downloadedBytes = undefined
+    this.downloadedBytes = undefined;
   }
 
   /** Downloads the updater package and installs it */
@@ -117,18 +119,18 @@ class Update extends Resource {
     convertToRustHeaders(options)
     const channel = new Channel<DownloadEvent>()
     if (onEvent) {
-      channel.onmessage = onEvent
+      channel.onmessage = onEvent;
     }
     await invoke('plugin:updater|download_and_install', {
       onEvent: channel,
       rid: this.rid,
       ...options
-    })
+    });
   }
 
   async close(): Promise<void> {
-    await this.downloadedBytes?.close()
-    await super.close()
+    await this.downloadedBytes?.close();
+    await super.close();
   }
 }
 
@@ -138,8 +140,8 @@ async function check(options?: CheckOptions): Promise<Update | null> {
 
   const metadata = await invoke<UpdateMetadata | null>('plugin:updater|check', {
     ...options
-  })
-  return metadata ? new Update(metadata) : null
+  });
+  return metadata ? new Update(metadata) : null;
 }
 
 /**
@@ -151,5 +153,6 @@ function convertToRustHeaders(options?: { headers?: HeadersInit }) {
   }
 }
 
-export type { CheckOptions, DownloadOptions, DownloadEvent }
-export { check, Update }
+export { check, Update };
+export type { CheckOptions, DownloadEvent, DownloadOptions };
+
