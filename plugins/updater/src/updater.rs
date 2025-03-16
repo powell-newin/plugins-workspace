abe81d7358d2f0c6621382950b8c855a070d8a53
powell-newin <52061817+powell-newin@users.noreply.github.com>
2025-03-16 12:38:36 +0900

    Use rustls-tls

--- plugins/updater/src/updater.rs
+++ plugins/updater/src/updater.rs
@@ -439,6 +439,7 @@ impl Updater {
             }
 
             let response = request
+                .use_rustls_tls()
                 .build()?
                 .get(url)
                 .headers(headers.clone())
@@ -636,6 +637,7 @@ impl Update {
             request = configure_client(request);
         }
         let response = request
+            .use_rustls_tls()
             .build()?
             .get(self.download_url.clone())
             .headers(headers)
